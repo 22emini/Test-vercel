dcbded9820d14d4c3db8f5940ce25dbd28df4335
22emini <eminioluwaakinrinade716@gmail.com>
2025-01-23 15:13:14 +0100

    123

---
+++ app/dashboard/_components/BarChartComponent.jsx
@@ -0,0 +1,51 @@
+import { getUniqueRecord } from '@/app/_services/service'
+import React, { useEffect, useState } from 'react'
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import{motion} from 'framer-motion'
+
+function BarChartComponent({attendaceList,totalPresentData}) {
+
+    const [data,setData]=useState([]);
+
+    useEffect(()=>{
+        setData([])
+        formatAttendanceListCount();
+    },[attendaceList||totalPresentData])
+
+    const formatAttendanceListCount=()=>{
+        const totalStudent=getUniqueRecord(attendaceList);
+        console.log(totalStudent.length,totalPresentData)
+        const result=totalPresentData.map((item=>({
+            day:item.day,
+            presentCount:item.presentCount,
+            absentCount:Number(totalStudent?.length)-Number(item.presentCount)
+        })));
+
+        console.log(result);
+        setData(result)
+    }
+  return (
+    <motion.div 
+    
+    initial ={{opacity: 0, scale: 0}}
+    whileInView={{opacity: 1, scale: 1}}
+    transition={{duration: 2, type:'spring'}}
+    
+    className='p-5 border rounded-lg shadow-sm'>
+        <h2 className='my-2 font-bold text-lg'>Attendance</h2>
+        <ResponsiveContainer width={'100%'} height={300}>
+    <BarChart  data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="day" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="presentCount" name="Total Present" fill="#4c8cf8" />
+        <Bar dataKey="absentCount" name="Total Absent" fill="#1fe6d1" />
+</BarChart>
+</ResponsiveContainer>
+    </motion.div>
+  )
+}
+
+export default BarChartComponent
